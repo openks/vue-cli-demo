3698ac38fa7c98d10bd75dc14e4a71894f613945
openks <zhuyangxing@foxmail.com>
2017-11-01 13:58:45 +0800

    init -设置css-module类名样式

---
+++ build/vue-loader.conf.js
@@ -0,0 +1,23 @@
+'use strict'
+const utils = require('./utils')
+const config = require('../config')
+const isProduction = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  loaders: utils.cssLoaders({
+    sourceMap: isProduction
+      ? config.build.productionSourceMap
+      : config.dev.cssSourceMap,
+    extract: isProduction
+  }),
+  cssModules: {
+    localIdentName: '[path][name]---[local]---[hash:base64:5]',
+    camelCase: true
+  },
+  transformToRequire: {
+    video: 'src',
+    source: 'src',
+    img: 'src',
+    image: 'xlink:href'
+  }
+}
